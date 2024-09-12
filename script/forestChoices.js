7654e52d56dba882aeb6b4d95f9879f60a194f32
Azes <ardonit.lajqi@gmail.com>
2024-09-12 11:50:58 +0200

    ja

--- script/forestChoices.js
+++ script/forestChoices.js
@@ -39,21 +39,3 @@ function ChoicesBoxDuplicater() {
 
     console.log(react.x, react.y);
 }
-
-ctx = document.getElementById("c").getContext("2d");
-ctx.beginPath();
-canvas_arrow(ctx, 0, 0, react.x, react.y);
-ctx.stroke();
-
-
-function canvas_arrow(context, fromx, fromy, tox, toy) {
-  var headlen = 10; // length of head in pixels
-  var dx = tox - fromx;
-  var dy = toy - fromy;
-  var angle = Math.atan2(dy, dx);
-  context.moveTo(fromx, fromy);
-  context.lineTo(tox, toy);
-  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
-  context.moveTo(tox, toy);
-  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
-}
