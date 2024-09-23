9be080a9cddb4c3e3772aa2f422f4d6320d2c234
Jacob-Schaub <Jacob.schaub@elev.ga.ntig.se>
2024-09-23 09:25:20 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,15 +2,21 @@
 const staminaBar = document.getElementById("stamina");
 const healthBar = document.getElementById("health");
 const manaBar = document.getElementById("mana");
+const wantedRating = document.getElementById("wantedRating");
+const coins = document.getElementById("coins");
 
 // Set initial values and styles
 staminaBar.style.width = "100%";
 healthBar.style.width = "100%";
 manaBar.style.width = "100%";
+wantedRating.innerHTML = "0";
+coins.innerHTML = "0";
 
 export let stamina = 10;
 export let health = 10;
 export let mana = 10;
+export let wanted = 0;
+export let coin = 0;
 
 export function setStamina(value) {
     stamina = value;
@@ -28,4 +34,14 @@ export function setMana(value) {
     mana = value;
     manaBar.innerHTML = value + " Mp";
     manaBar.style.width = (value * 10) + "%"; // Adjust percentage as needed
+}
+
+export function setCoins(value) {
+    coin = value;
+    coins.innerHTML = value;
+}
+
+export function setWanted(value) {
+    wanted = value;
+    wantedRating.innerHTML = value + "/100";
 }
