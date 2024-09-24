0a019b5f5c315afa709442d4c594803917dd2111
Azes <ardonit.lajqi@gmail.com>
2024-09-24 11:28:35 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -45,3 +45,27 @@ export function setWanted(value) {
     wanted = value;
     wantedRating.innerHTML = value + "/100";
 }
+
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) 
+    {
+        mybutton.style.display = "block";
+    } 
+  else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
