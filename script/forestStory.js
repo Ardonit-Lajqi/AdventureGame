dce68fe8451f556e093e02b4cd17995bf334d6ad
Azes <ardonit.lajqi@gmail.com>
2024-10-08 14:55:54 +0200

    Update forestStory.js

--- script/forestStory.js
+++ script/forestStory.js
@@ -4,7 +4,7 @@ export let storyNode = {
         question: "Where do you want to go?",
         choices: [
             { text: "Hut", next: "hut"},
-            { text: "Continue Forward", next: "OutsideCity" },
+            { text: "Continue Forward", specialAction: true, id:"continueForward" },
             { text: "Deeper Forest", next: "startDeepForest" }
         ]
     },
@@ -91,6 +91,15 @@ export let storyNode = {
         ]
     },
 
+    forestCity: {
+        text: "You pass a small town in the forest",
+        question: "What do you want to do?",
+        choices: [
+            { text: "look around",}, //Behöver fixa SpecialAction för lämna Staden
+            { text: "investigate"} // investigera har inte fixat än
+        ]
+    },
+
     startDeepForest: {
         text: "You enter a deep part of the forest, You feel it will be easy to get lost here",
         question: "Where do you wanna go?",
@@ -98,6 +107,41 @@ export let storyNode = {
             { text: "Forward", next: "forwardF"},
             { text: "Right", next: "rightR"},
             { text: "Left", next: "leftL"}
+        ]
+    },
+
+    forwardF: {
+        text: "You go forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardFF"},
+            { text: "Right", next: "rightFR"}
+        ]
+    },
+
+    forwardFF: {
+        text: "You go forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardFFF"},
+            { text: "Left", next: "rightFFL"}
+        ]
+    },
+
+    forwardFFF: {
+        text: "You go Forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardFFFF"}
+        ]
+    },
+
+    forwardFFFF: {
+        text: "You go Forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardFFFFF"},
+            { text: "Left", next: "leftFFFFL"},
         ]
     },
 
@@ -120,12 +164,85 @@ export let storyNode = {
         ]
     },
 
+    rightLFR: {
+        text: "You go Right",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Right", next: "forwardF"},
+            { text: "Left", next: "leftLFRL"}
+        ]
+    },
+
+    leftLFRL: {
+        text: "You go Left",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Left", next: "leftLFFFR"},
+            { text: "Forward", specialAction: true, id: "LFRL"} // Får röd mossa
+        ]
+    },
+
+    leftLFFFR: {
+        text: "You go Left",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", specialAction: true, id: "LFFFRF"},
+            { text: "Left", specialAction: true, id: "FallDownInPit" }
+        ]
+    },
+
+    forwardLFFFRF: {
+        text: "You go Forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Right", next: "rightLFFFRFL"}
+        ]
+    },
+
     forwardLFF: {
         text: "You go Forward",
         question: "Where do you wanna go?",
         choices: [
             { text: "Left", specialAction: true, id: "DeepForestStart"},
-            { text: "Forward", next: "forwardLFFF"}
+            { text: "Forward", next: "forwardLFFF"},
+            { text: "Right", next: "rightFFL"}
+        ]
+    },
+
+    forwardLFFF: {
+        text: "You go Forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardLFFFF"},
+            { text: "Left", next: "hut"},
+            { text: "Right", next: "leftLFFFR"}
+        ]
+    },
+
+    forwardLFFFF: {
+        text: "You go Forward",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardLLF" },
+            { text: "Left", specialAction: true, id: "startDeepForest"}
+        ]
+    },
+
+    rightFFL: {
+        text: "You go right",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Forward", next: "forwardLFF"},
+            { text: "Left", next: "rightR"},
+            { text: "Right", next: "rightFFFFL"}
+        ]
+    },
+
+    rightFFFFL: {
+        text: "You go Right",
+        question: "Where do you wanna go?",
+        choices: [
+            { text: "Right", specialAction: true, id:"DeepForestExit"},
         ]
     },
 
@@ -139,7 +256,7 @@ export let storyNode = {
         ]
     },
 
-    leftLLF: {
+    forwardLLF: {
         text: "You go foward",
         question: "Where do you wanna go?",
         choices: [
@@ -153,7 +270,7 @@ export let storyNode = {
         question: "Where do you wanna go?",
         choices: [
             { text: "Right", specialAction: true, id:"DeepForestStart"},
-            { text: "Back", next: "backLF" }
+            { text: "Back", next: "forwardLF" }
         ]
     },
 
